7d3f809a552ebce35702dc4379041e51a1a9b40e
Muhammad Naveed <mnaveed47012@gmail.com>
2023-01-07 22:46:58 +0500

    Add dynamic styles

--- src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
+++ src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
+// import './CourseGoalItem.css';
 
-import './CourseGoalItem.css';
+const ListItem = styled.li`
+  margin: 1rem 0;
+  background: #8b005d;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  color: white;
+  padding: 1rem 2rem;
+  cursor: pointer;
+`;
 
 const CourseGoalItem = props => {
   // const [deleteText, setDeleteText] = useState('');
@@ -10,10 +19,16 @@ const CourseGoalItem = props => {
     props.onDelete(props.id);
   };
 
+  // return (
+  //   <li className="goal-item" onClick={deleteHandler}>
+  //     {props.children}
+  //   </li>
+  // );
+
   return (
-    <li className="goal-item" onClick={deleteHandler}>
+    <ListItem className="goal-item" onClick={deleteHandler}>
       {props.children}
-    </li>
+    </ListItem>
   );
 };
 
